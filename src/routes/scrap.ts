46da808f3f9cb6170821adcd6bc171fe5bfd8ed8
pratikgrv <pratikgaurav37@gmail.com>
2024-06-09 22:50:20 +0530

    init project

--- src/routes/scrap.ts
+++ src/routes/scrap.ts
@@ -5,82 +5,86 @@ const router = express.Router()
 
 router.get('/', async (req: Request, res: Response) => {
     const API_URL = 'https://api.producthunt.com/v2/api/graphql'
-    const api_token_ph = process.env.PRODUCT_HUNT_API_TOKEN // Replace with your Product Hunt API token
+    const api_token_ph = process.env.PRODUCT_HUNT_API_TOKEN
+
+    if (!api_token_ph) {
+        console.error('API token is not defined')
+        return res.status(500).send({
+            error: true,
+            message: 'API token is not defined',
+        })
+    }
 
     const query = {
         query: `
-        query todayPosts {
-            posts {
-                totalCount
-                edges {
-                    node {
-                        id
-                        name
-                        tagline
-                        votesCount
-                        featuredAt
-                        makers {
-                            name
-                            followers {
-                                totalCount
-                            }
-                            following {
-                                totalCount
-                            }
-                            madePosts {
-                                totalCount
-                            }
-                            twitterUsername
-                        }
-                    }
-                }
-            }
+        query {
+            posts(first: 10, order: RANKING) {
+    edges {
+      node {
+        id
+        name
+        tagline
+        votesCount
+        featuredAt
+        createdAt
+       
+        reviewsCount
+        reviewsRating
+        commentsCount
+        makers {
+          id
+          name
+          username
+          twitterUsername
+          websiteUrl
+          profileImage
         }
-    `,
+        media {
+          type
+          url
+        }
+        
+      }
     }
-    async function fetchProducts() {
-        try {
-            const response = await axios.post(
-                API_URL,
-                { query },
-                {
-                    headers: {
-                        Accept: 'application/json',
-                        'Content-Type': 'application/json',
-                        Authorization: 'Bearer ' + api_token_ph,
-                        Host: 'api.producthunt.com',
-                    },
-                },
-            )
-
-            const { data } = response.data
-            console.log(data)
-
-            // if (data && data.categories) {
-            //     data.categories.edges.forEach((categoryEdge) => {
-            //         const category = categoryEdge.node
-            //         console.log(`Category: ${category.name}`)
-
-            //         category.products.edges.forEach((productEdge) => {
-            //             const product = productEdge.node
-            //             console.log(`  Product: ${product.name}`)
-            //             console.log(`    Tagline: ${product.tagline}`)
-            //             console.log(`    Votes: ${product.votesCount}`)
-            //         })
-            //     })
-            // } else {
-            //     console.error('No data found')
-            // }
-        } catch (error) {
-            console.error('Error fetching products:', error)
+  }
         }
+        `,
     }
 
-    fetchProducts()
+    try {
+        const response = await axios.post(API_URL, query, {
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + api_token_ph,
+            },
+        })
 
-    return res.status(200).send({
-        message: '👋 Hello, World! scrap',
-    })
+        console.log(response.data) // Log the entire response for debugging
+
+        const data = response.data
+        if (data && data.data) {
+            return res.status(200).send({
+                success: true,
+                data: data.data, // Send back the fetched data
+            })
+        } else {
+            console.error('Unexpected response structure', data)
+            return res.status(500).send({
+                error: true,
+                message: 'Unexpected response structure',
+            })
+        }
+    } catch (error) {
+        console.error(
+            'Error fetching products:',
+            error.response ? error.response.data : error.message,
+        )
+        return res.status(500).send({
+            error: true,
+            message: 'Error fetching products',
+        })
+    }
 })
 
 export default router
